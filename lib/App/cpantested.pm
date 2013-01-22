93b659e253e9cd2d617288dc88f6fb0c13dbd846
Stanislaw Pusep <creaktive@gmail.com>
2013-01-22 12:46:44 -0200

    INIT

---
+++ lib/App/cpantested.pm
@@ -0,0 +1,30 @@
+package App::cpantested;
+# ABSTRACT: see cpan-tested
+
+=head1 SYNOPSIS
+
+    cpan-outdated | cpan-tested | cpanm
+
+=head1 DESCRIPTION
+
+...
+
+=cut
+
+use 5.008;
+use strict;
+use utf8;
+use warnings qw(all);
+
+# VERSION
+
+=head1 SEE ALSO
+
+=for :list
+* L<App::cpanoutdated>
+* L<App::cpanminus>
+* L<App::cpantimes>
+
+=cut
+
+1;
