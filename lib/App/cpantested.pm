9760baf80b6ec7f23619fead37674e234a96b8a2
Stanislaw Pusep <creaktive@gmail.com>
2013-01-22 13:40:49 -0200

    better description

--- lib/App/cpantested.pm
+++ lib/App/cpantested.pm
@@ -1,5 +1,5 @@
 package App::cpantested;
-# ABSTRACT: see cpan-tested
+# ABSTRACT: delegate testing to the cloud
 
 =head1 SYNOPSIS
 
@@ -8,6 +8,13 @@ package App::cpantested;
 =head1 DESCRIPTION
 
 Pre-filter the output from the L<cpan-outdated> utility, crossing it with the results from the L<CPAN Testers Reports|http://cpantesters.org/>.
+
+By default, considers "installable" a distribution that has any test that:
+
+=for :list
+* Has a B<PASS> grade;
+* Has the same B<Perl version> as the target system;
+* Has the same B<OS name> as the target system.
 
 =cut
 
